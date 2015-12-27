7b893abe6e4e9788b17d1b859740fa8dac16df26
Shane Ray <shane.ray87@gmail.com>
2015-12-27 12:17:52 -0500

    Updated config service to allow configUrl to be overridden by VSTS and updated web.release.config

--- src/ShaneSpace.GameSite.Web/js/Services/ConfigService.js
+++ src/ShaneSpace.GameSite.Web/js/Services/ConfigService.js
@@ -1,11 +1,16 @@
 ﻿angular.module("ConfigService", [])
     .factory('Config', [
     function () {
+        var configUrl = 'http://localhost:62614'
+
+        var configOverride = '$(GameServiceUrl)';
+        if (configOverride.indexOf("GameServiceUrl") === -1) {
+            configUrl = configOverride;
+        }
+
         var Config = {
-            GameServiceConfig: { domain: 'http://localhost:62614' },
-            GameServiceUrl: "http://localhost:62614"
-            //GameServiceConfig: { domain: 'http://shanespace.duckdns.org:2023' },
-            //GameServiceUrl: "http://shanespace.duckdns.org:2023"
+            GameServiceConfig: { domain: configUrl },
+            GameServiceUrl: configUrl
         };
 
         return Config;
